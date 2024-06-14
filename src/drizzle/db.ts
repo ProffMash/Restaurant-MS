5e56560ed8dce724d1e50a8d80dfe4ca90779dc7
david macharia <kdavidmacharia8485@gmail.com>
2024-06-14 15:38:49 +0300

    updated db

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -3,6 +3,9 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema"
+import assert from "assert"
+
+assert(process.env.DATABASE_URL, "DATABASE_URL is not set in the .env file")
 
 export const sql = neon (process.env.Database_URL as string)  //get the database url from the environment)
 
